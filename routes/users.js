9123b9cf6edb08cb9ec5e00fc0206ee5e0ae71ca
CripBoy <pedro-lucas63@hotmail.com>
2019-04-21 11:29:09 -0300

    routers

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+/*
+*	todo user routers
+*	@query param
+*/
+router.get('/register', function(req, res, next) {
+	res.send('respond with a resource');
+});
+
+router.get('/autenticar', function(req, res, next) {
+	res.send('respond with a resource');
+});
+
+
+/*
+*	todo task routers
+*	@query param
+*/
+router.get('/create', function(req, res, next) {
+	res.send('respond with a resource');
+});
+
+router.get('/update', function(req, res, next) {
+	res.send('respond with a resource');
+});
+
+router.get('/remove', function(req, res, next) {
+	res.send('respond with a resource');
+});
+
+router.get('/list', function(req, res, next) {
 	res.send('respond with a resource');
 });
 
