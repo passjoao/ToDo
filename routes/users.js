7c8275100db68ed65e2a9aff5958a63d271c0277
CripBoy <pedro-lucas63@hotmail.com>
2019-04-21 13:18:06 -0300

    router insert

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var usuario = require('../model/Usuario');
 
 /*
 *	todo user routers
 *	@query param
 */
 router.get('/register', function(req, res, next) {
-	res.end();
+	var usuario = {
+		nome: req.param.nome,
+		email: req.param.email, 
+		senha: req.param.senha
+	}
+
+	usuario.register(usuario, function(){
+		console.log('registrado')
+	});
 });
 
 router.get('/autenticar', function(req, res, next) {
